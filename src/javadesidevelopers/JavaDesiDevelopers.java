97d451a4817a270ab2109beb791c3610ab02c6c0
user <user@user-PC>
2017-01-19 07:11:01 +0530

    Created stringPower skeleton.

--- src/javadesidevelopers/JavaDesiDevelopers.java
+++ src/javadesidevelopers/JavaDesiDevelopers.java
@@ -16,6 +16,12 @@ public class JavaDesiDevelopers {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    
+    
+    }
+    public static String stringPower(String s,int a)
+    {
+        return s;
     }
     
 }
